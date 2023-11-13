66b9770ccbfbd23a170246d7ce5b363dc4dc5d58
LinkTheCoder <66062268+LinkTheCoder@users.noreply.github.com>
2023-11-13 12:37:40 +0100

    qoutes update

--- commands/skills/quotes.js
+++ commands/skills/quotes.js
@@ -1,29 +1,46 @@
 const { SlashCommandBuilder } = require('discord.js');
+const fs = require('fs');
 
-// Define an array of image file paths
-const imagePaths = [
-    './img/quotes/MorganaQuote.png',
-    './img/quotes/RyujiQuote.png',
-    './img/quotes/FutabaQuote.png',
-    // Add more image paths as needed
-];
+// Read the data from the JSON file
+const rawData = fs.readFileSync('data.json');
+const data = JSON.parse(rawData);
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('quotes')
-        .setDescription('Replies with a random image!'),
-    async execute(interaction) {
-        // Select a random image path
-        const randomImagePath = imagePaths[Math.floor(Math.random() * imagePaths.length)];
+        .setDescription('Get a random quotes from P5 characters')
+        .addStringOption(option =>
+            option
+                .setName('character')
+                .setDescription('Select the character')
+                .setRequired(true)
+                .addChoices(
+                    { name: 'Morgana', value: 'morgana' },
+                    { name: 'Ryuji', value: 'ryuji' },
+                    { name: 'Futaba', value: 'futaba' },
+                )
+        ),         
 
-        // Send only the random image as a response
-        await interaction.reply({
-            files: [
-                {
-                    attachment: randomImagePath,
-                    name: 'RandomImage.png', // You can set a fixed name for the image
-                },
-            ],
-        });
+    async execute(interaction) {
+        const character = interaction.options.getString('character');
+        const characterQuotes = data[character];
+
+        if (characterQuotes) {
+            // Select a random quote image path for the specified character
+            const randomQuotePath =
+                characterQuotes[Math.floor(Math.random() * characterQuotes.length)];
+
+            // Send the random quote image as a response
+            await interaction.reply({
+                files: [
+                    {
+                        attachment: randomQuotePath,
+                        name: `${character}Quote.png`,
+                    },
+                ],
+            });
+        } else {
+            await interaction.reply('Invalid character.');
+        }
     },
 };
